7ad4e4d8ea0ce25f256784c47b135c236c67f657
Marcus Klausen <marcusklausen@gmail.com>
2017-10-16 19:09:38 +0200

    Initial Commit

---
+++ iTunesClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunesClient
+//
+//  Created by Marcus Klausen on 16/10/2017.
+//  Copyright © 2017 Marcus Klausen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
